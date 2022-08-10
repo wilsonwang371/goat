d9161449c283a902b70353fbf9db3ea814ef2516
Wilson Wang <wilsonny371@gmail.com>
2022-08-10 10:07:37 -0700

    minor fix

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 if [ -z "${IN_DOCKER}" ]
 then
     # We are not in docker, so we need to run the build script in docker.
-    docker run --rm --env "IN_DOCKER=1"  -v "${SCRIPT_DIR}:/goalgotrade:rw" ${BUILD_IMG} /goalgotrade/build.sh
+    docker run --rm --env "IN_DOCKER=1"  -v "${SCRIPT_DIR}:/goalgotrade:rw" ${BUILD_IMG} /goalgotrade/build.sh $1
 else
     # We are in docker, so we can run the build script directly.
     go install mvdan.cc/gofumpt@latest
