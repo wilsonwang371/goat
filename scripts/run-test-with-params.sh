bfa3f99883ac2777becd7912efb6bf3525fd4753
Wilson Wang <wilsonny371@gmail.com>
2022-08-17 09:46:47 -0700

    put params in a file

--- scripts/run-test-with-params.sh
+++ scripts/run-test-with-params.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-for i in "GOAT_EMAIL_HOST" \
-"GOAT_EMAIL_PORT" \
-"GOAT_EMAIL_USER" \
-"GOAT_EMAIL_PASSWORD" \
-"GOAT_EMAIL_FROM" \
-"GOAT_EMAIL_TO"; do
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+params=$(cat ${SCRIPT_DIR}/params.txt)
+
+for i in ${params}; do
     read -p "Enter $i: " $i
     eval "export $i=\${$i}"
 done
