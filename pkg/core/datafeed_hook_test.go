381a2117673398d5d06a678feeb42411e285f948
Wilson Wang <wilsonny371@gmail.com>
2022-09-04 10:41:43 -0700

    update test

--- pkg/core/datafeed_hook_test.go
+++ pkg/core/datafeed_hook_test.go
@@ -2,9 +2,16 @@ package core
 
 import (
 	"fmt"
+	"math"
 	"testing"
 	"time"
 )
+
+const float64EqualityThreshold = 1e-2
+
+func almostEqual(a, b float64) bool {
+	return math.Abs(a-b) <= float64EqualityThreshold
+}
 
 func TestHookSimple(t *testing.T) {
 	ctrl := NewDataFeedValueHookControl()
@@ -48,5 +55,57 @@ func TestHookSimple2(t *testing.T) {
 	fmt.Printf("%+v\n", v)
 	if v == nil {
 		t.Error("should not be nil")
+	}
+}
+
+func TestHookVerify(t *testing.T) {
+	ctrl := NewDataFeedValueHookControl()
+	ctrl.AddNewHook(NewDayBarGenHook())
+
+	for i := 1; i < 10; i++ {
+		tm := time.Date(2016, time.January, 1, i, 0, 0, 0, time.UTC)
+		barData := NewBasicBar(
+			tm,
+			100*float64(i),
+			100*float64(i),
+			100*float64(i),
+			100*float64(i),
+			100*float64(i),
+			0, REALTIME)
+		ctrl.FilterNewValue(&PendingDataFeedValue{
+			t: tm,
+			f: REALTIME,
+			v: map[string]interface{}{
+				"test": barData.(interface{}),
+			},
+		}, false)
+		barData2 := NewBasicBar(
+			tm,
+			-100*float64(i),
+			-100*float64(i),
+			-100*float64(i),
+			-100*float64(i),
+			-100*float64(i),
+			0, REALTIME)
+		ctrl.FilterNewValue(&PendingDataFeedValue{
+			t: tm,
+			f: REALTIME,
+			v: map[string]interface{}{
+				"test": barData2.(interface{}),
+			},
+		}, false)
+	}
+
+	v := ctrl.PossibleOneNewValue()
+	if v == nil {
+		t.Error("should not be nil")
+	}
+
+	res := v.v["test"].(Bar)
+
+	if !almostEqual(res.Open(), 100.000) || !almostEqual(res.Close(), -900.000) ||
+		!almostEqual(res.High(), 900.000) || !almostEqual(res.Low(), -900.000) {
+		tmp := fmt.Sprintf("open %f, close %f, high %f, low %f\n", res.Open(), res.Close(), res.High(), res.Low())
+		t.Error("verify failed", tmp)
 	}
 }
