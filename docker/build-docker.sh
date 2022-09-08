7c62573ce40d60a9512d5b33abb01a8c403ae4e2
Wilson Wang <wilson.wang@bytedance.com>
2022-09-08 13:47:40 -0700

    update

--- docker/build-docker.sh
+++ docker/build-docker.sh
@@ -13,7 +13,7 @@ fi
 ${BUILDX_CMD} create --use --name goat-builder 
 
 ${BUILDX_CMD} build \
-    --load \
+    --cache-to=type=local,dest=./  \
     --platform=linux/amd64,linux/arm64 \
     --tag=wilsonny/goat-build:latest -f ${SCRIPT_DIR}/build.dockerfile ${SCRIPT_DIR}/..
 
@@ -22,7 +22,7 @@ if [ ! -f ${SCRIPT_DIR}/goat-arm64 ] || [ ! -f ${SCRIPT_DIR}/goat-amd64 ] ; then
 fi
 
 ${BUILDX_CMD} build \
-    --load \
+    --cache-to=type=local,dest=./ \
     --platform=linux/amd64,linux/arm64 \
     --tag=wilsonny/goat-release:latest -f ${SCRIPT_DIR}/release.dockerfile ${SCRIPT_DIR}/..
 
